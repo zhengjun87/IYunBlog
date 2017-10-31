07bc09503cf64cb3b897d5185baa649a298c54c7
zhengjun <zhengjun@ekeyfund.com>
2017-10-31 11:55:42 +0800

    修改

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use(session({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", req.Origin);
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By",' 3.2.1')
