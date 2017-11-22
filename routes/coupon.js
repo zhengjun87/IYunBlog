0e1e403eed52dd01fdb3e76d5168f0d7b613eb5e
zhengjun <zhengjun@ekeyfund.com>
2017-11-22 18:16:49 +0800

    修改响应

--- routes/coupon.js
+++ routes/coupon.js
@@ -2,7 +2,7 @@ var express = require('express');
 var couponmodel = require('../model/db').coupon;
 var router = express.Router();
 router.get('/', function(req, res, next) {
-  res.send('000');
+  res.send('地址更改（将www修改为quan）请访问：http://quan.woiyun.com/coupon/#/aa8888');
 });
 router.post('/getCoupon', function(req, res, next) {
   var body=req.body;
