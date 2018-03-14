ab5f3743c166f881c1d23e502d3be5806c56ae39
zhengjun <zhengjun@ekeyfund.com>
2018-03-14 14:48:49 +0800

    添加headers允许content-type

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(session({
 app.use(express.static(path.join(__dirname, 'public')));
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin",req.headers.origin);
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, content-type, Accept");
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
