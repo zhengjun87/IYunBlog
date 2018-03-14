0e182b0c7687ae4299721a6cd151b42d3b83025b
zhengjun <zhengjun@ekeyfund.com>
2018-03-14 14:02:16 +0800

    修改请求方法支持options

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin",req.headers.origin);
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
 });
 app.use('/', index);
